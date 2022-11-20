ae61ca5d7c85aa238efc4c497a1cdc7267684e67
Buffik <Som16@mail.ru>
2022-11-20 22:01:17 +0300

    feat: add page with results

--- songbird/src/pages/result/result.js
+++ songbird/src/pages/result/result.js
@@ -1,1 +1,23 @@
-console.log('result');
+import '../../assets/sass/result.scss';
+import backgroundVideo from '../../assets/media/video/background_video.mp4';
+import renderResultTemplate from '../../assets/js/components/result/resultTemplate';
+
+const videoWrapper = document.createElement('div');
+videoWrapper.classList.add('background__wrapper');
+
+const videoBlock = document.createElement('video');
+videoBlock.autoplay = true;
+videoBlock.muted = true;
+videoBlock.loop = true;
+videoBlock.src = backgroundVideo;
+videoBlock.type = 'video/mp4';
+videoBlock.classList.add('background__video');
+
+videoWrapper.append(videoBlock);
+document.body.prepend(videoWrapper);
+
+const currentLanguage = localStorage.getItem('lang');
+const currentScore = Number(localStorage.getItem('score'));
+const parentNode = document.querySelector('main');
+
+renderResultTemplate(parentNode, currentLanguage, currentScore);
